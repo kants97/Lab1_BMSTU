26fe4b5af794ae5fad17ed4293ce9eb0b2831873
Lab5UIVC210-212 <kants1997@live.com>
2020-02-21 19:36:19 +0300

    Var 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,25 +5,38 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        int n;
+        int n, k;
+        boolean flag = false;
+        int[] array = new int[50];
+
         Scanner in = new Scanner(System.in);
         System.out.print("Input n: ");
         n = in.nextInt();
 
-        String[] str = new String[n];
         for (int i=0; i<n; i++) {
-            str[i] = in.next();
+            array[i] = in.nextInt();
+        }
+
+        while (flag == false) {
+            flag = true;
+            for (int i=0; i<n-1; i++) {
+                if (array[i]>array[i+1]){
+                    flag = false;
+                    k = array[i];
+                    array[i] = array[i+1];
+                    array[i+1] = k;
+                }
+            }
         }
 
         for (int i=0; i<n; i++) {
-            System.out.print(str[i]);
+            System.out.print(array[i] + " ");
         }
 
         System.out.println();
 
-        for (int i=0; i<n; i++) {
-            System.out.println(str[i]);
+        for (int i=n; i>0; i--) {
+            System.out.print(array[i-1] + " ");
         }
-
     }
 }
